a81a2454d215e6f01287e5ff5a869a244ab3f3fc
shainova <shainova@gmail.com>
2018-01-31 13:17:29 -0600

    1. Change how data is read due to new data set structure; 2. Other small improvements

--- accumulation_curves_midway.R
+++ accumulation_curves_midway.R
@@ -1,9 +1,29 @@
+# Arguments ---------------------------------------------------------------
 args <- commandArgs(trailingOnly=TRUE)
 ups_arg <- as.character(args[1])
-run_arg <- as.numeric(args[2])
-threshold_arg <- as.numeric(args[3])
+threshold_arg <- as.numeric(args[2])
+MOI <- as.numeric(args[3])
+data_file <- as.character(args[4])
+ 
+# data_file <- 'For_Shai_1099_isolates_764_types_at_70%_seq_ID.csv'
+run_arg <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
-# Initialize --------------------------------------------------------------
+# Functions --------------------------------------------------------------
+prep.packages = function(package.list) {
+  loaded = package.list %in% .packages()
+  if ( all(loaded) ) return(invisible())
+  
+  package.list = package.list[!loaded]
+  installed = package.list %in% .packages(TRUE)
+  if ( !all(installed) ) install.packages(package.list[!installed], repos="http://cran.rstudio.com/")
+  for ( p in package.list )
+  {
+    print(paste("Loading package:",p))
+    suppressMessages(library(p,character.only=TRUE))
+  }
+}
+
+# This function is taken from the bipartite package
 empty <- function (web, count = FALSE) {
   web[is.na(web)] <- 0
   if (NCOL(web) == 1 | NROW(web) == 1) {
@@ -40,42 +60,59 @@ empty <- function (web, count = FALSE) {
   return(out)
 }
 
+subsetDataMOI <- function(data_input, MOI, min_repertoire_length=20){
+  if (MOI>=100){
+    data_MOI <- data_input
+  } else {
+    maxVars <- MOI*60
+    data_MOI <- data_input[,colSums(data_input)>=min_repertoire_length & colSums(data_input)<=maxVars]
+    data_MOI <- empty(data_MOI)
+    dim(data_MOI)
+    print(paste('MOI<=',MOI,' | ',ncol(data_MOI),' repertoires | ',nrow(data_MOI),' vars',sep=''))
+  }
+  return(data_MOI)  
+}
 
+
+# Libraries ---------------------------------------------------------------
+library(data.table)
+library(stringr)
+
+# Load data ---------------------------------------------------------------
+data <- fread(paste('Data/',data_file,sep=''))
 # Get A/BC groups
-dataUps <- read.csv('S1 S2 All isolates 20 or more DBLa types ASYMP RESERVOIR A NON A PRELIM.csv')
-dataUps <- dataUps[,1:2]
+dataUps <- data[,1:2]
 table(dataUps$Ups)
-#setequal(dataUps$Type,vars)
-# dataUps <- dataUps[match(vars,dataUps$Type),]
-#all(dataUps$Type==vars)
+# Get DBLa types
+DBLa <- data[,1]
+# Make a matrix
+data <- data[,-c(1,2)]
+data <- data.matrix(data)
+rownames(data) <- DBLa$DBLa_type
 
+# The distribution of repertoire lengthis based on MOI=1 and is per ups A/BC group
 if (ups_arg!='ABC'){
   print(ups_arg)
-  data <- read.csv('S1 S2 Single Infections 20-60 DBLa types ASYMP RESERVOIR.csv', row.names = 1)
-  data <- data.matrix(data)
-  data <- data[rownames(data)%in%subset(dataUps, Ups==ups_arg)$Type,]
-  data <- empty(data)
-  repertoireLengthDistribution <- table(colSums(data))
-  data <- read.csv('S1 S2 All isolates 20 or more DBLa types ASYMP RESERVOIR.csv', row.names = 1)
-  data <- data.matrix(data)
-  data <- data[rownames(data)%in%subset(dataUps, Ups==ups_arg)$Type,]
+  dataMOI1 <- subsetDataMOI(data, 1)
+  dataMOI1 <- dataMOI1[rownames(dataMOI1)%in%subset(dataUps, Ups==ups_arg)$DBLa_type,]
+  dataMOI1 <- empty(dataMOI1)
+  repertoireLengthDistribution <- table(colSums(dataMOI1))
+  # Now subsample the data to obtain only the desired ups group (A or BC)
+  data <- data[rownames(data)%in%subset(dataUps, Ups==ups_arg)$DBLa_type,]
   data <- empty(data)
 } else {
   print('ABC')
-  data <- read.csv('S1 S2 Single Infections 20-60 DBLa types ASYMP RESERVOIR.csv', row.names = 1)
-  data <- data.matrix(data)
-  repertoireLengthDistribution <- table(colSums(data))
-  data <- read.csv('S1 S2 All isolates 20 or more DBLa types ASYMP RESERVOIR.csv', row.names = 1)
-  data <- data.matrix(data)
+  dataMOI1 <- subsetDataMOI(data, 1)
+  repertoireLengthDistribution <- table(colSums(dataMOI1))
 }
 
-range(data)
 vars <- rownames(data)
 
 
 
 # Run curves --------------------------------------------------------------
-makeCurve <- function(dat,threshold=0.95,subsample_vars){
+# Subsampling vars is to create a lower boundry to the curve
+makeCurve <- function(dat, threshold=0.95, subsample_vars){
   R <- colnames(dat) # The set of repertoires
   V <- rownames(dat) # The set of vars
   cumulativeSampledVars <- c()
@@ -112,15 +149,20 @@ makeCurve <- function(dat,threshold=0.95,subsample_vars){
   return(data.frame(bites=0:bites,propVars=cumulativeNumberSampledVars/length(V)))
 }
 
-message('Loading data...')
+pos <- str_locate(data_file,'%')[1]
+resolution <- str_sub(data_file,pos-2,pos-1)
 
+print('Making curves with subsample...')
 tmp <- makeCurve(data, threshold_arg, subsample_vars=T)
 tmp$run <- run_arg
 tmp$Ups <- ups_arg
-write.table(tmp, paste('curveData_',ups_arg,'_',threshold_arg,'_subsampleT_',run_arg,'.csv',sep=''),row.names = F, sep=',')
+tmp$subsample <- T
+write.table(tmp, paste('../Results/curveData_',ups_arg,'_',threshold_arg,'_subsampleT_',resolution,'_run_',run_arg,'.csv',sep=''),row.names = F, sep=',')
 
+print('Making curves without subsample...')
 tmp <- makeCurve(data, threshold_arg, subsample_vars=F)
 tmp$run <- run_arg
 tmp$Ups <- ups_arg
-write.table(tmp, paste('curveData_',ups_arg,'_',threshold_arg,'_subsampleF_',run_arg,'.csv',sep=''),row.names = F, sep=',')
+tmp$subsample <- F
+write.table(tmp, paste('../Results/curveData_',ups_arg,'_',threshold_arg,'_subsampleF_',resolution,'_run_',run_arg,'.csv',sep=''),row.names = F, sep=',')
   
