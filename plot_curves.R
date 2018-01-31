259e0564659765b86f638d87d121ac9732d2a619
shainova <shainova@gmail.com>
2018-01-31 14:30:22 -0600

    Fixed to plot by resolution and Ups

--- plot_curves.R
+++ plot_curves.R
@@ -1,8 +1,10 @@
+# Initialize --------------------------------------------------------------
 library(ggplot2)
-setwd('/home/shai/Documents/Shazia/Results')
-getRuns <- function(Ups, resolution){
+library(data.table)
+
+getRuns <- function(Ups, resolution, iterations){
   lower_curve <- upper_curve <- c()
-  for (i in 1:50){
+  for (i in 1:iterations){
     print(i)
     lower_curve <- rbind(lower_curve, fread(paste('curveData_',Ups,'_0.95_subsampleT_',resolution,'_run_',i,'.csv',sep=''), he=T))
     upper_curve <- rbind(upper_curve, fread(paste('curveData_',Ups,'_0.95_subsampleF_',resolution,'_run_',i,'.csv',sep=''), he=T))
@@ -12,6 +14,37 @@ getRuns <- function(Ups, resolution){
   curveData <- rbind(lower_curve,upper_curve)
   return(curveData)
 }
+
+
+# Get data ----------------------------------------------------------------
+setwd('/home/shai/Documents/Shazia/Results')
+d <- NULL
+for (r in c(70,80,90,96)){
+  for (u in (c('A','BC','ABC'))){
+    x <- getRuns(u,r,25)
+    d <- rbind(d, x)
+  }
+}
+
+
+# PLot --------------------------------------------------------------------
+d$Ups <- factor(d$ups, levels = c('ABC','BC','A'))
+EIR=100
+ggplot(d, aes(bites/EIR, propVars, color=subsample))+
+  geom_smooth(se = T, method = "gam", formula = y ~ s(log(x+1)))+
+  theme_bw()+
+  scale_color_manual(values=c('springgreen4','brown'))+
+  labs(x=paste('Years (assuming EIR of ',EIR,')',sep=''), y='Proportion of DBLa types accumulated')+
+  # facet_wrap(~Ups,scales='free')
+  facet_grid(Ups~resolution, scales='free')
+
+
+
+
+
+
+
+
 curveData_A <- getRuns('A',96)
 curveData_BC <- getRuns('BC',96)
 curveData_ABC <- getRuns('ABC',96)
